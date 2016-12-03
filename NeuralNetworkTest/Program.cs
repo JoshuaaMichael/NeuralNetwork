687e0ed9748be1c7aaed473f97336032ca6a991e
Joshua Cameron <joshuaa.michael@gmail.com>
2016-12-03 13:56:59 +1100

    Removed testing csv, it's now not complex, using a library for CSV reading. Split the two file processing classes out under an abstract class, made ImportableData interface

--- NeuralNetworkTest/Program.cs
+++ NeuralNetworkTest/Program.cs
@@ -6,35 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//I think a CSV reading library would be better
-
 namespace NeuralNetworkTest
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			int numberInputs = 5, numberOutputs = 4;
-			int numberOfRecords = 100;
-			double count = 1.0125;
 
-			List<string> fileGen = new List<string>();
-
-			for(int i = 0; i < numberOfRecords; i++)
-			{
-				string line = "";
-				for (int j = 0; j < numberInputs; j++)
-				{
-					line += count++ + ", ";
-				}
-				for (int j = 0; j < numberOutputs; j++)
-				{
-					line += (j != numberInputs - 1) ? count++ + ", " : count++ + "";
-				}
-				fileGen.Add(line);
-			}
-
-			File.WriteAllLines("generated.txt", fileGen.ToArray());
 		}
 	}
 }
