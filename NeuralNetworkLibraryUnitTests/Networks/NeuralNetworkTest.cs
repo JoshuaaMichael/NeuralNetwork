978cc9fc47ff68bb6e0de7668ffd3f9cd3a5f027
Joshua Cameron <joshuaa.michael@gmail.com>
2016-12-03 16:39:51 +1100

    Started on testing neural network tests

---
+++ NeuralNetworkLibraryUnitTests/Networks/NeuralNetworkTest.cs
@@ -0,0 +1,32 @@
+﻿using NeuralNetworkLibrary.ActivationFunctions;
+using NeuralNetworkLibrary.NetworkNodes;
+using NeuralNetworkLibrary.Networks;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkLibraryUnitTests.Networks
+{
+	[TestFixture]
+	class NeuralNetworkTest
+	{
+
+		[OneTimeSetUp]
+		protected void Init()
+		{
+		}
+
+		[SetUp]
+		protected void SetUp()
+		{
+		}
+
+		[Test]
+		public void Test()
+		{
+		}
+	}
+}
